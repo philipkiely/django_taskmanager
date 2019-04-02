78fdecd29d87a814cf02a3c714ad5cb87a6a3317
Philip Kiely <kielyphi@grinnell.edu>
2019-04-02 17:20:28 +0200

    api implemented, not tested

--- taskmanager/taskmanager/urls.py
+++ taskmanager/taskmanager/urls.py
@@ -1,21 +1,13 @@
-"""taskmanager URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('all/', views.all_tasks, name='all_tasks'),
+    path('new/', views.new_task, name='new_task'),
+    path('update/', views.update_task, name='update_task'),
+    path('delete/', views.delete_task, name='delete_task'),
+    path('signin/', views.signin, name='signin'),
 ]
--- taskmanager/todo/views.py
+++ taskmanager/todo/views.py
@@ -1,3 +1,109 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework.status import HTTP_400_BAD_REQUEST
+# More rest imports as needed
+from django.contrib.auth import authenticate
+from datetime import date, timedelta
+from .decorators import define_usage
+from .models import Task
 
-# Create your views here.
+
+#URL /
+@define_usage(returns={'url_usage': 'Dict'})
+@api_view(['GET'])
+@permission_classes((AllowAny,))
+def index(requet):
+    details = {}
+    for item in list(globals().items()):
+        if item[0][0:4] == 'api_':
+            if hasattr(item[1], 'usage'):
+                details[reverse(item[1].__name__)] = item[1].usage
+    return Response(details)
+
+
+#URL /signin/
+#Note that in a real Django project, signin and signup would most likely be
+#handled by a seperate app. For signup on this example, use the admin panel.
+@define_usage(params={'username': 'String', 'password': 'String'},
+              returns={'authenticated': 'Bool', 'token': 'Token String'})
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def signin(request):
+    try:
+        username = request.data['username']
+        password = request.data['password']
+    except:
+        return Response({'error': 'Please provide correct username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({'authenticated': True, 'token': token.key})
+    else:
+        return Response({'authenticated': False, 'token': None})
+
+
+#URL /all/
+@define_usage(returns={'tasks': 'Dict'})
+@api_view(['GET'])
+def all_tasks(request):
+    try:
+        return Response({'tasks': request.user.task_list.all()})
+    except:
+        return Response({'error': 'Please provide correct token'},
+                        status=HTTP_400_BAD_REQUEST)
+
+
+#URL /new/
+@define_usage(params={'description': 'String', 'due_in': 'Int'},
+              returns={'done': 'Bool'})
+@api_view(['PUT'])
+def new_task(request):
+    try:
+        task = Task(user=request.user,
+                    description=request.data['description'],
+                    due=date.now() + timedelta(days=int(request.data['due_in'])))
+        task.save()
+        return Response({'done': True})
+    except:
+        return Response({'error': 'Please provide correct token'},
+                        status=HTTP_400_BAD_REQUEST)
+
+
+#URL /update/
+@define_usage(params={'task_id': 'Int', 'description': 'String', 'due_in': 'Int'},
+              returns={'done': 'Bool'})
+@api_view(['POST'])
+def update_task(request):
+    try:
+        task = request.user.task_list.get(id=int(request.data['task_id']))
+        try:
+            task.description = request.data['description']
+        except: #Description update is optional
+            pass
+        try:
+            task.due = date.now() + timedelta(days=int(request.data['due_in']))
+        except: #Due date update is optional
+            pass
+        task.save()
+        return Response({'done': True})
+    except:
+        return Response({'error': 'Please provide correct token'},
+                        status=HTTP_400_BAD_REQUEST)
+
+
+#URL /delete/
+@define_usage(params={'task_id': 'Int'},
+              returns={'done': 'Bool'})
+@api_view(['DELETE'])
+def delete_task(request):
+    try:
+        task = request.user.task_list.get(id=int(request.data['task_id']))
+        task.delete()
+        return Response({'done': True})
+    except:
+        return Response({'error': 'Please provide correct token'},
+                        status=HTTP_400_BAD_REQUEST)
